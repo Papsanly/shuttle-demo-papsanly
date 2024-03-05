137e4724c1caaa678132d7ad842bc0c8fbf58651
Papsanly <papsanlysenko@gmail.com>
2024-03-05 21:22:51 +0200

    add secret_store to main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use axum::{routing::get, Router};
+use shuttle_secrets::SecretStore;
 
 async fn hello_world() -> &'static str {
     "Bye, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main(#[shuttle_secrets::Secrets] _secret_store: SecretStore) -> shuttle_axum::ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
-
     Ok(router.into())
 }
