2353d4b394fa2be7af55fc5b71f73a04612cc5dc
Papsanly <papsanlysenko@gmail.com>
2024-03-05 23:21:16 +0200

    add unimplemented handlers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-use axum::{routing::get, Router};
+use axum::extract::{Path, State};
+use axum::{
+    response::{Redirect, Result},
+    routing::{get, post},
+    Router,
+};
 use shuttle_axum::ShuttleAxum;
 use shuttle_runtime::CustomError;
 use shuttle_shared_db::Postgres;
 use sqlx::{Executor, PgPool};
+use std::sync::Arc;
 
-async fn hello_world() -> &'static str {
-    "Bye, world!"
+struct AppState {
+    pool: PgPool,
+}
+
+async fn redirect(Path(id): Path<u32>, State(state): State<Arc<AppState>>) -> Redirect {
+    unimplemented!()
+}
+
+async fn shorten(State(state): State<Arc<AppState>>, url: String) -> String {
+    unimplemented!()
 }
 
 #[shuttle_runtime::main]
@@ -14,6 +28,12 @@ async fn main(#[Postgres] pool: PgPool) -> ShuttleAxum {
         .await
         .map_err(CustomError::new)?;
 
-    let router = Router::new().route("/", get(hello_world));
+    let state = Arc::new(AppState { pool });
+
+    let router = Router::new()
+        .route("/", post(shorten))
+        .route("/", get(redirect))
+        .with_state(state);
+
     Ok(router.into())
 }
