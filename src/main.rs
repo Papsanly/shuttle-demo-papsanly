db508c78cce035587e969401a5962def7b54cb1d
Papsanly <papsanlysenko@gmail.com>
2024-03-05 21:32:37 +0200

    add use statements

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use axum::{routing::get, Router};
-use shuttle_secrets::SecretStore;
+use shuttle_axum::ShuttleAxum;
+use shuttle_secrets::{SecretStore, Secrets};
 
 async fn hello_world() -> &'static str {
     "Bye, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main(#[shuttle_secrets::Secrets] _secret_store: SecretStore) -> shuttle_axum::ShuttleAxum {
+async fn main(#[Secrets] _secret_store: SecretStore) -> ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
     Ok(router.into())
 }
