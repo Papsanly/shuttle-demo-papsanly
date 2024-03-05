9dddc647dbc43a55338658187fc92fe09d0766cc
Papsanly <papsanlysenko@gmail.com>
2024-03-05 21:58:50 +0200

    add postgres pool

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use axum::{routing::get, Router};
 use shuttle_axum::ShuttleAxum;
+use shuttle_runtime::CustomError;
+use shuttle_shared_db::Postgres;
+use sqlx::{Executor, PgPool};
 
 async fn hello_world() -> &'static str {
     "Bye, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> ShuttleAxum {
+async fn main(#[Postgres] pool: PgPool) -> ShuttleAxum {
+    pool.execute(include_str!("schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
+
     let router = Router::new().route("/", get(hello_world));
     Ok(router.into())
 }
