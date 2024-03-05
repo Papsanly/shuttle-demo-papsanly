1aeb9c668e3bcf32549fc666de50f3d6602837ed
Papsanly <papsanlysenko@gmail.com>
2024-03-05 21:43:25 +0200

    remove shuttle secrets

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use axum::{routing::get, Router};
 use shuttle_axum::ShuttleAxum;
-use shuttle_secrets::{SecretStore, Secrets};
 
 async fn hello_world() -> &'static str {
     "Bye, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main(#[Secrets] _secret_store: SecretStore) -> ShuttleAxum {
+async fn main() -> ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
     Ok(router.into())
 }
